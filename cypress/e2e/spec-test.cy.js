51a1ed22f6be7f50de57aa7ca8c34213d7dffea6
Matthew Dacre <matthewd@bbd.co.za>
2023-09-27 16:42:39 +0200

    Checkpoint commit

---
+++ cypress/e2e/spec-test.cy.js
@@ -0,0 +1,73 @@
+describe("Page Loads and buttons present", () => {
+  it("Visits the calculator", () => {
+    cy.visit("localhost:3000");
+    for (let i = 0; i < 10; i++) {
+      cy.get(".card-body").contains(i.toString());
+    }
+
+    const customChars = [
+      "√",
+      "AC",
+      "(",
+      ")",
+      "÷",
+      "-",
+      "+",
+      "=",
+      "%",
+      "π",
+      "!",
+    ];
+    for (let c = 0; c < customChars.length; c++) {
+      cy.get(".card-body").contains(customChars[c]);
+    }
+  });
+});
+
+describe("Enter a calculation and observe results and history", () => {
+  it("Adds 1+1, expects 2 and clear to leave 0", () => {
+    cy.visit("localhost:3000");
+
+    cy.get("#one").click();
+    cy.get("#add").click();
+    cy.get("#one").click();
+    cy.get("#equal").click();
+
+    cy.get("#display").contains("2");
+
+    cy.get("#clear").click();
+
+    cy.get("#display").contains("0");
+  });
+
+  it("Checks history logs 1+1=2 to history, and clears", () => {
+    cy.visit("localhost:3000");
+
+    cy.get("#one").click();
+    cy.get("#add").click();
+    cy.get("#one").click();
+    cy.get("#equal").click();
+
+    cy.get("#historyDisplay").contains("1+1");
+    cy.get("#historyDisplay").contains("2");
+
+    cy.get("#clearBtn").click();
+
+    cy.get("#historyDisplay").contains("No history");
+  });
+
+  it("Checks fill from history", () => {
+    cy.visit("localhost:3000");
+
+    cy.get("#one").click();
+    cy.get("#add").click();
+    cy.get("#one").click();
+    cy.get("#equal").click();
+
+    cy.get("#clear").click();
+
+    cy.get('.list-group-item[question="1+1"]').click();
+
+    cy.get("#display").contains("2");
+  });
+});
