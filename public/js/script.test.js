b125936931e6f060125f27530612de5d6246882a
Daelin Campleman <daelin.campleman@bbd.co.za>
2023-09-28 12:10:19 +0200

    Fixed sqrt of negative

--- public/js/script.test.js
+++ public/js/script.test.js
@@ -2,7 +2,7 @@ import { calculateEquation } from './script';
 
 describe("Square root", () => {
     test('"√(9)" should result in 3', () => {
-        expect(calculateEquation("√(8)")).toBe(3);
+        expect(calculateEquation("√(9)")).toBe(3);
     });
     test('"√(4)" should result in 2', () => {
         expect(calculateEquation("√(4)")).toBe(2);
@@ -13,8 +13,8 @@ describe("Square root", () => {
     test('"√(0)" should result in 0', () => {
         expect(calculateEquation("√(0)")).toBe(0);
     });
-    test('"√(-4)" should result in NaN', () => {
-        expect(calculateEquation("√(-4)")).toBe(NaN);
+    test('"√(-4)" should result in Error: Negative square root', () => {
+        expect(calculateEquation("√(-4)")).toBe("Error: Negative square root");
     });
     test('"√(2)" should result in 1.414', () => {
         expect(calculateEquation("√(2)")).toBe(1.4142135624);
