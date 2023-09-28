2af3141eaa070885c92751fd526568391771b01f
Janco Venter <jan@bbd.co.za>
2023-09-28 08:56:58 +0200

    subtraction and division tests

---
+++ dom-mock.js
@@ -0,0 +1,10 @@
+const mockElement = {
+    innerHTML: '',
+  };
+
+  document.getElementById = jest.fn((id) => {
+    if (id === 'previousQuestion' || id === 'display') {
+      return mockElement;
+    }
+    return null;
+  });
---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    testEnvironment: 'jest-environment-jsdom', // Set the test environment to jest-environment-jsdom
+    setupFiles: ['./dom-mock'],
+  };
