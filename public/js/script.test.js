5e755bc7d1075d8a7c4846eb075f2049d9a79dcf
Raaga Krishna <raaga@bbd.co.za>
2023-09-28 11:20:32 +0200

    Fixed typo

--- public/js/script.test.js
+++ public/js/script.test.js
@@ -13,9 +13,9 @@ describe("Square root", () => {
     test('"√(0)" should result in 0', () => {
         expect(calculateEquation("√(0)")).toBe(0);
     });
-    // test('"√(-4)" should result in NaN', () => {
-        // expect(calculateEquation("√(-4)")).toBe(NaN));
-    // });
+    test('"√(-4)" should result in NaN', () => {
+        expect(calculateEquation("√(-4)")).toBe(NaN);
+    });
     test('"√(2)" should result in 1.414', () => {
         expect(calculateEquation("√(2)")).toBe(1.414);
     });
