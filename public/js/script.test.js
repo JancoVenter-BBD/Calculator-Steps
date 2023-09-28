67c2ce57dadbdfc65ef2460ed6471745731418e7
Matthew Dacre <matthewd@bbd.co.za>
2023-09-28 11:44:11 +0200

    Merge main

--- public/js/script.test.js
+++ public/js/script.test.js
@@ -18,3 +18,35 @@ describe("Addition", () => {
     });
 });
 
+describe("Subtraction", () => {
+    test('"5-5" should result in 0', () => {
+      expect(calculateEquation("5-5")).toBe(0);
+    });
+    test('"5--5" should result in 10', () => {
+      expect(calculateEquation("5--5")).toBe(10);
+    });
+    test('"0-10" should result in -10', () => {
+      expect(calculateEquation("0-10")).toBe(-10);
+    });
+    test('"3.5-1.2" should result in 2.3', () => {
+      expect(calculateEquation("3.5-1.2")).toBe(2.3);
+    });
+});
+
+describe("Division", () => {
+  test('"10/2" should result in 5', () => {
+    expect(calculateEquation("10/2")).toBe(5);
+  });
+  test('"10/0" should result in an "Error: Division by zero"', () => {
+    expect(calculateEquation("10/0")).toBe("Error: Division by zero");
+  });
+  test('"2.5/5" should result in 0.5', () => {
+    expect(calculateEquation("2.5/5")).toBe(0.5);
+  });
+});
+
+describe("BODMAS chain operation", () => {
+  test('"2^((2+2)×3!)/(4*2)" should result in 2097152', () => {
+    expect(calculateEquation("2^((2+2)×3!)/(4*2)")).toBe(2097152);
+  });
+})
